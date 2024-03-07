54f73ad96cbbfbf76593b1a8457646581b4bff31
milotriss <nhattienla98@gmail.com>
2024-03-07 16:28:18 +0700

    cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,15 +7,14 @@ import createEntity from "./entities/index.entites";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import sequelize from "./configs/db.config";
-import { Server, Socket } from 'socket.io';
-import http from 'http';
-
+import { Server, Socket } from "socket.io";
+import http from "http";
 
 dotenv.config();
 const server = express();
 server.use(
   cors({
-    origin: ["huongbakery-cake.vercel.app","http://localhost:3000"],
+    origin: ["https://huongbakery-cake.vercel.app/", "http://localhost:3000"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
@@ -23,24 +22,23 @@ server.use(
 const app = http.createServer(server);
 const io = new Server(app, {
   cors: {
-      origin: ["huongbakery-cake.vercel.app","http://localhost:3000"]
+    origin: ["https://huongbakery-cake.vercel.app/", "http://localhost:3000"],
   },
 });
-io.on('connection',(socket:Socket)=>{
-  console.log(socket.id,"socket id",'Client connection');
-  socket.on('blockUser',(id:number)=>{
-    io.emit('logout',id);
-  })
-  socket.on('createReview',(id:number)=>{
+io.on("connection", (socket: Socket) => {
+  console.log(socket.id, "socket id", "Client connection");
+  socket.on("blockUser", (id: number) => {
+    io.emit("logout", id);
+  });
+  socket.on("createReview", (id: number) => {
     // console.log(id);
-    io.emit('updateReview',id);
-  })
-  socket.on('statusPayment',(data:any)=>{
+    io.emit("updateReview", id);
+  });
+  socket.on("statusPayment", (data: any) => {
     // console.log(data);
-    io.emit('statusHistory',data);
-  })
-  
-})
+    io.emit("statusHistory", data);
+  });
+});
 const port = process.env.PORT;
 server.use(express.static("public"));
 server.use(bodyParser.json());
@@ -57,6 +55,4 @@ server.use(
 sequelize.authenticate();
 routers(server);
 // createEntity()
-app.listen(port, () =>
-  console.log(`http://localhost:${port} SERVER OKK FEN`)
-);
+app.listen(port, () => console.log(`http://localhost:${port} SERVER OKK FEN`));
